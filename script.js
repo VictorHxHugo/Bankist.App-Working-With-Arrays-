65fb21a602329e8b8e45f588bbb60ad8fe085ddc
VictorHxHugo <victorzk6@hotmail.com>
2023-12-06 17:10:15 -0300

    forEach With Maps and Sets

--- script.js
+++ script.js
@@ -130,9 +130,10 @@ console.log('jonas'.at(-1));
 */
 
 /////////////////////////////////////////////////
+/*
 // Looping Arrays: forEach
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 // for of
 
@@ -152,4 +153,28 @@ movements.forEach(function (mov, i) {
   } else {
     console.log(`Movement ${i + 1}: You withdraw ${Math.abs(mov)}`);
   }
+});
+*/
+
+/////////////////////////////////////////////////
+
+//  forEach With Maps and Sets
+
+// Map
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+currencies.forEach(function (value, key, map) {
+  console.log(`${key} ${value}`);
+});
+
+// Set
+const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+console.log(currenciesUnique);
+
+currenciesUnique.forEach(function (value, _, map) {
+  console.log(`${value}: ${value}`);
 });
