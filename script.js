430902a4f1c3c15a8c1e1f61c4f28d49d0b63132
VictorHxHugo <victorzk6@hotmail.com>
2023-12-14 13:28:42 -0300

    The find Method

--- script.js
+++ script.js
@@ -393,8 +393,6 @@ Rewrite the 'calcAverageHumanAge' function from the previous challenge, but this
 TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
 TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
 
-*/
-
 const calcAverageHumanAge = ages =>
   ages
     .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
@@ -405,3 +403,15 @@ const calcAverageHumanAge = ages =>
 const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
 const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
 console.log(avg1, avg2);
+*/
+
+///////////////////////////////////////
+// The find Method
+
+const firstWithdrawal = movements.find(mov => mov < 0);
+console.log(movements);
+console.log(firstWithdrawal);
+
+console.log(accounts);
+const account = accounts.find(acc => acc.owner === 'Jessica Davis');
+console.log(account);
